8e23d8f8f360fa77063c202ae305b2e24b0a680d
GitHub Action <action@github.com>
2021-08-13 16:22:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/akiacode/copilot/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/akiacode/copilot/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.akiacode.copilot.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akiacode.copilot.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/akiacode/copilot/services/MyProjectService.kt
+++ src/main/kotlin/com/github/akiacode/copilot/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.akiacode.copilot.services
 
+import com.github.akiacode.copilot.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
